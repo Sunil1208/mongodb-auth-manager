2088d76dc854bf80924bf26328c6ad22062c19b9
Sunil1208 <skmy03@gmail.com>
2020-04-19 18:01:17 +0530

    "Project Initialized,database connected, and environment variables made"

---
+++ index.js
@@ -0,0 +1,57 @@
+const cors = require('cors');
+const express = require("express");
+const bodyParser = require('body-parser');
+const {
+    connect
+} = require('mongoose');
+const {
+    success,
+    error
+} = require('consola')
+
+//App constants
+const {
+    DB,
+    PORT
+} = require('./config');
+
+//Initialize the application
+
+const app = express()
+
+//MiddleWare
+app.use(cors());
+app.use(bodyParser.json());
+
+//Use Router middleWare
+app.use('/api/users',require('./routes/users'));
+
+//Connection with the database
+
+const startApp = async () =>{
+    
+    try {
+        await connect(DB, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useFindAndModify: true
+        });
+        success({
+            message: `Successfully connected with the Database \n${DB}`,
+            badge:true
+        })
+        
+        app.listen(PORT, () => success({
+            message: `Server up and running on PORT ${PORT}`,
+            badge: true
+        }));
+
+    } catch (err) {
+        error({
+            message: `Unable to connect with the database\n{err}`,
+            badge:true
+        })
+        startApp()
+    }  
+}
+startApp();
